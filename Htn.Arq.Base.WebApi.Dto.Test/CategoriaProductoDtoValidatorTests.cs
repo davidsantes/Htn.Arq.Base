2bacd0dde06515a1753d1f5f411fba7e28d39a5e
davidsantes <30901115+davidsantes@users.noreply.github.com>
2023-09-17 14:08:34 +0200

    FUNC - Testing en validadores

---
+++ Htn.Arq.Base.WebApi.Dto.Test/CategoriaProductoDtoValidatorTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentValidation.TestHelper;
+using Htn.Arq.Base.WebApi.Dto.Validators;
+using System.Resources;
+using Xunit;
+using Htn.Arq.Base.WebApi.Dto.Resources;
+
+namespace Htn.Arq.Base.WebApi.Dto.Test
+{
+    [Trait("Categoria", "ValidacionCategoriaProductoDto")]
+    public class CategoriaProductoDtoValidatorTests
+    {
+        private readonly ResourceManager _resourceManager;
+
+        public CategoriaProductoDtoValidatorTests()
+        {
+            _resourceManager = new ResourceManager(typeof(ValidationResources));
+        }
+
+        [Fact]
+        public void Dado_CategoriaProductoDtoValidator_CuandoValoresObligatoriosSiInformados_EntoncesOk()
+        {
+            // Arrange
+            var validator = new CategoriaProductoDtoValidator();
+            var categoria = new CategoriaProductoDto
+            {
+                Id = 1,
+                Nombre = "Nombre de categoría válido"
+            };
+
+            // Act
+            var result = validator.TestValidate(categoria);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void Dado_CategoriaProductoDtoValidator_CuandoValoresObligatoriosNoInformados_EntoncesKo()
+        {
+            // Arrange
+            var validator = new CategoriaProductoDtoValidator();
+            var categoria = new CategoriaProductoDto
+            {
+                Id = 1,
+                Nombre = "" // Nombre vacío, debería fallar
+            };
+
+            var categoriaNombreRequerido = _resourceManager.GetString("CategoriaNombreRequerido");
+
+            // Act
+            var result = validator.TestValidate(categoria);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(c => c.Nombre)
+                .WithErrorMessage(categoriaNombreRequerido);
+        }
+
+        [Fact]
+        public void Dado_CategoriaProductoDtoValidator_CuandoValoresObligatoriosMuyLargos_EntoncesKo()
+        {
+            // Arrange
+            var validator = new CategoriaProductoDtoValidator();
+            var categoria = new CategoriaProductoDto
+            {
+                Id = 1,
+                Nombre = "Nombre de categoria demasiado extenso para la cantidad de caracteres aceptados"
+            };
+         
+            int maxLength = 50;
+            var categoriaNombreRequerido = string.Format(_resourceManager.GetString("CategoriaNombreMaxLength"), maxLength);
+
+            // Act
+            var result = validator.TestValidate(categoria);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(c => c.Nombre)
+                .WithErrorMessage(categoriaNombreRequerido);
+        }
+    }
+}
--- Htn.Arq.Base.WebApi.Dto/Validators/CategoriaProductoDtoValidator.cs
+++ Htn.Arq.Base.WebApi.Dto/Validators/CategoriaProductoDtoValidator.cs
@@ -1,14 +1,23 @@
 ﻿using FluentValidation;
+using Htn.Arq.Base.WebApi.Dto.Resources;
+using System.Resources;
 
 namespace Htn.Arq.Base.WebApi.Dto.Validators
 {
     public class CategoriaProductoDtoValidator : AbstractValidator<CategoriaProductoDto>
     {
+        private readonly ResourceManager _resourceManager;
+
         public CategoriaProductoDtoValidator()
         {
+            //TODO: revisar si hacerlo con IStringLocalizer para inyectarlo
+            _resourceManager = new ResourceManager(typeof(ValidationResources));
+
+            int maxLength = 50;
+            
             RuleFor(categoria => categoria.Nombre)
-                .NotEmpty().WithMessage("El nombre de la categoría es obligatorio.")
-                .MaximumLength(50).WithMessage("El nombre de la categoría no puede tener más de 50 caracteres.");
+                .NotEmpty().WithMessage(_resourceManager.GetString("CategoriaNombreRequerido"))              
+                .MaximumLength(maxLength).WithMessage(string.Format(_resourceManager.GetString("CategoriaNombreMaxLength"), maxLength));
         }
     }
 }
