6c782a967c46549b49d49e747e5f0f9e2cb7caf5
davidsantes <30901115+davidsantes@users.noreply.github.com>
2023-10-21 10:46:32 +0200

    FUNC - TODO: httpClient: https://www.milanjovanovic.tech/blog/the-right-way-to-use-httpclient-in-dotnet

--- Fuentes/Hacienda.Infrastructure/Adapters/CorreosAdapter.cs
+++ Fuentes/Hacienda.Infrastructure/Adapters/CorreosAdapter.cs
@@ -5,6 +5,12 @@ namespace Hacienda.Infrastructure.Adapters
 {
     public class CorreosAdapter : ICorreosAdapter
     {
+        /// <summary>
+        /// TODO: Revisar uso de httpclient:
+        /// https://www.milanjovanovic.tech/blog/the-right-way-to-use-httpclient-in-dotnet
+        /// Replacing Named Clients With Typed Clients
+        /// Why You Should Avoid Typed Clients In Singleton Services (indicarlo!!!!)
+        /// </summary>
         public async Task<Result<bool>> InsAsync()
         {
             // Simulamos una operación asíncrona de creación, como una inserción en la base de datos
