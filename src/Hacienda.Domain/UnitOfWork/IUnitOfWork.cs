8ac0f754eecfdb053bda1832bc5d53fe64100f73
d695839 <david.santesteban.herrero@navarra.es>
2023-10-26 11:52:21 +0200

    FUNC - añadir contrato para Unit

---
+++ src/Hacienda.Domain/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿namespace Hacienda.Domain.UnitOfWork;
+
+/// <summary>
+/// Unit of work para persistencia de datos con repositorios.
+/// </summary>
+public interface IUnitOfWork
+{
+    Task<int> CompleteAsync();
+
+    int Complete();
+
+    void BeginTransaction();
+
+    void CommitTransaction();
+
+    void RollbackTransaction();
+}
