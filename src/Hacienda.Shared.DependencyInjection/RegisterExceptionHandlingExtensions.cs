3972a9bdf358914ab23fb2eb60f2204c88933a7b
davidsantes <30901115+davidsantes@users.noreply.github.com>
2023-10-26 22:44:45 +0200

    FUNC -  Extensiones para iny de dependencias, unificar criterio para excepciones

--- src/Hacienda.Shared.DependencyInjection/RegisterExceptionHandlingExtensions.cs
+++ src/Hacienda.Shared.DependencyInjection/RegisterExceptionHandlingExtensions.cs
@@ -1,5 +1,8 @@
-﻿using Hacienda.Shared.Core.Exceptions.Middlewares;
+﻿using Hacienda.Application.Exceptions.Sanitize;
+using Hacienda.Application.ProblemDetails;
+using Hacienda.Shared.Core.Exceptions.Middlewares;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Hacienda.Shared.DependencyInjection;
 
@@ -15,5 +18,20 @@ public static class RegisterExceptionHandlingExtensions
     {
         app.UseMiddleware<ExceptionHandlingMiddleware>();
         return app;
+    }
+
+    /// <summary>
+    /// Registra en el contenedor de DI las políticas de saneamiento de excepciones y los problem details
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <returns>Colección configurada</returns>
+    public static IServiceCollection RegisterExceptionAndProblemDetails(
+        this IServiceCollection services)
+    {
+        //Debe ser singleton porque se utiliza en un middlewares, el cual por defecto es singleton.
+        services.AddSingleton<IExceptionPolicy, SanitizeNotControlledExceptionsPolicy>();
+        services.AddSingleton<IProblemDetailsFactory, ProblemDetailsFactory>();
+
+        return services;
     }
 }
