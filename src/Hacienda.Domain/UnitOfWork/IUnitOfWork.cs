42136c44eb1c05778c6179d004f6e4b4e3d42f46
davidsantes <30901115+davidsantes@users.noreply.github.com>
2023-11-03 12:34:23 +0100

    FUNC -  eliminar UOW

--- src/Hacienda.Domain/UnitOfWork/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hacienda.Domain.UnitOfWork;
-
-/// <summary>
-/// Unit of work para persistencia de datos con repositorios.
-/// </summary>
-public interface IUnitOfWork
-{
-    Task<int> CommitAsync();
-}
