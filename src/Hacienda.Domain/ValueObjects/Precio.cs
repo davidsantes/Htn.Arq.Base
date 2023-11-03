1bcea6b527bab04d4513df6bd69ef53f9c7f3c3a
davidsantes <30901115+davidsantes@users.noreply.github.com>
2023-11-03 09:53:25 +0100

    REF -  Cambiar el nombre

---
+++ src/Hacienda.Domain/ValueObjects/Precio.cs
@@ -0,0 +1,8 @@
+﻿namespace Hacienda.Domain.ValueObjects;
+
+/// <summary>
+/// Se podría utilizar por ejemplo, en el caso detalle de un pedido o productos, para el precio.  
+/// Se trata de un Value object, ya que de por sí no nos dice nada, a quién aplica, etcétera.
+/// Los records son simples, pero no permite validaciones, se deberían hacer en el padre.
+/// </summary>
+public record class Precio(string moneda, decimal cantidad);
